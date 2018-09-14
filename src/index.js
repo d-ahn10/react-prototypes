87fc7b5f914bfd4176bc1c11ff677664dd1088c4
David Ahn <dja1990@gmail.com>
2018-09-14 00:30:01 -0700

    React Intro - David Ahn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { userInfo } from 'os';
+
+const luckyNumber = () => Math.floor((Math.random() * 1000) + 1);
+
+const greeting = user => (
+        <div className="container">
+            <h1>Hello {user.name}</h1>
+            <h2 className="text-muted">Your lucky number is {user.luckyNumber}</h2>
+        </div>
+)
+
+const user = {
+    name: 'David',
+    luckyNumber: luckyNumber()
+}
 
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    //<h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
